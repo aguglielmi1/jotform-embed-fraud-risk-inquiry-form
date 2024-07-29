098c61202b538e7e7367b663f3c8cc1af3554456
Potluck Mittal <potluck@copilot.com>
2024-07-29 12:40:08 -0400

    get jotform embed working

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,15 +9,12 @@ export function middleware(request: NextRequest) {
   // If you have a custom domain add it below to the
   // space separated frame-ancestors list.
   const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data:;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
     form-action 'self';
-    frame-ancestors https://dashboard.copilot.com/ https://*.copilot.app/;
+    frame-ancestors https://dashboard.copilot.com/ https://*.copilot.app/ https://team.copilot1.com/;
     block-all-mixed-content;
     upgrade-insecure-requests;
 `;
